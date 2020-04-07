f83acd21e60650dbaa1d6ab9a03f0f819e7d45e2
aliumujib <aliuabdulmujib@yahoo.com>
2020-04-07 14:04:53 +0100

    Yaaay, completed Huffman encoding, now to decode

---
+++ project2/problem_3.py
@@ -0,0 +1,185 @@
+import heapq
+
+
+class HeapNode:
+
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def set_left_child(self, left):
+        self.left = left
+
+    def set_right_child(self, right):
+        self.right = right
+
+    def get_left_child(self):
+        return self.left
+
+    def get_right_child(self):
+        return self.right
+
+    def has_left_child(self):
+        return self.left is not None
+
+    def has_right_child(self):
+        return self.right is not None
+
+    def __repr__(self):
+        s = "{}".format(self.value)
+        return s
+
+    def __le__(self, other):
+        le = self.value[1] <= other.value[1]
+        return le
+
+    def __lt__(self, other):
+        lt = self.value[1] < other.value[1]
+        return lt
+
+    def __gt__(self, other):
+        gt = self.value[1] > other.value[1]
+        return gt
+
+    def __cmp__(self, other):
+        if other is None:
+            return -1
+        if not isinstance(other, HeapNode):
+            return -1
+        return self.value[1] > other.value[1]
+
+    def display(self):
+        lines, _, _, _ = self._display_aux()
+        for line in lines:
+            print(line)
+
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = "{}".format(self.value)
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = "{}".format(self.value)
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = "{}".format(self.value)
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = "{}".format(self.value)
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+
+# define a Tree class here
+class Tree:
+
+    def __init__(self, root):
+        self.root = root
+
+    def get_root_node(self):
+        return self.root
+
+
+class HuffmanCoding:
+
+    # An interesting thing I learnt here is comparisons, I started out with a generic node that took tuples but
+    # didn't account for how they will be compared while being used on the heap i.e no impl for _lt_ so the heap kept
+    # returning items at the front of the list. I also learnt about heaps which are a better alternative that the queue
+    # that I had to keep sorting in my first implementation.
+    def __build_tree__(self, chars_freq):
+        if len(chars_freq) == 1:
+            last_node = heapq.heappop(chars_freq)
+            return Tree(last_node)
+        else:
+            item_1 = heapq.heappop(chars_freq)
+            item_2 = heapq.heappop(chars_freq)
+            # $& special character to mark none root tuples
+            parent = HeapNode(("$&", item_1.value[1] + item_2.value[1]))
+            parent.set_left_child(item_1)
+            parent.set_right_child(item_2)
+            heapq.heappush(chars_freq, parent)
+            return self.__build_tree__(chars_freq)
+
+    def __make_map_of_key_codes__(self, current_string, result_map, node):
+        if not node.left and not node.right:
+            result_map[node.value[0]] = current_string
+        else:
+            self.__make_map_of_key_codes__("{}{}".format(current_string, "0"), result_map, node.left)
+            self.__make_map_of_key_codes__("{}{}".format(current_string, "1"), result_map, node.right)
+
+    def __encode_data_using_map__(self, data, result, map):
+        print(data)
+        if len(data) == 0:
+            return result
+        else:
+            result = result + map[data[0]]
+            return self.__encode_data_using_map__(data[1:], result, map)
+
+    def huffman_encoding(self, data):
+        character_frequencies = self.__count_character_frequencies__(data)
+        #print("type {}".format(type(character_frequencies)))
+        huffman_tree = self.__build_tree__(character_frequencies)
+        huffman_tree.get_root_node().display()
+        key_code_map = {}
+        self.__make_map_of_key_codes__("", key_code_map, huffman_tree.get_root_node())
+        print(key_code_map)
+        encoded_string = self.__encode_data_using_map__(data, "", key_code_map)
+        return encoded_string, key_code_map, huffman_tree
+
+    def huffman_decoding(self, data, tree):
+        pass
+
+    def __count_character_frequencies__(self, string):
+        map_of_frequencies = {}
+
+        for character in string:
+            if character in map_of_frequencies:
+                map_of_frequencies[character] = map_of_frequencies[character] + 1
+            else:
+                map_of_frequencies[character] = 1
+
+        list_of_frequencies = [HeapNode(item) for item in sorted(map_of_frequencies.items(),
+                                                                 key=lambda item: item[1])]
+
+        return list_of_frequencies
+
+
+huffmanCoding = HuffmanCoding()
+print("RES MAP: {}".format(huffmanCoding.huffman_encoding(
+    "only to get a representation of a dictionary that is sorted. Dictionaries are inherently orderless")))
